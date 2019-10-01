31d662ff297e111efadd9de3af4c65945aacfb3f
Mladen Petrov <mladen.petrov88@gmail.com>
2019-10-01 13:53:31 +0300

    Add files via upload

---
+++ TriplesOfLatinLetters.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TriplesOfLatinLetters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < number; j++) {
+                for (int k = 0; k < number; k++) {
+                    char firstChar = (char) ('a' + i);
+                    char secondChar = (char) ('a' + j);
+                    char thirdChar = (char) ('a' + k);
+                    System.out.printf("%c%c%c%n", firstChar, secondChar, thirdChar);
+                }
+            }
+        }
+    }
+}
